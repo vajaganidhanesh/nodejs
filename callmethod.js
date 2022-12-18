7d2ea4780a4a8926ceb07371c305e4d0a52a9502
vajagani dhanesh <vajaganidhanesh@gmail.com>
2022-12-18 07:16:23 +0530

    practice done

--- callmethod.js
+++ callmethod.js
@@ -17,9 +17,37 @@ let name1 = {
     secondname :"node js"
 }
 
-fullName.call(name2,"chittoor","andhra")
-fullName.apply(name1,["programming","client-side"])
+// fullName.call(name2,"chittoor","andhra")
+// fullName.apply(name1,["programming","client-side"])
 
 let fullnameBind = fullName.bind(name1,'programming','server-side');
-fullnameBind();
+// fullnameBind();
 
+// let string = 'dhanesh'
+// string[0]='D'
+// console.log(string);
+
+
+let arr1 = [1,2,3];
+let arr2 = arr1;
+
+console.log(arr1===arr2);
+
+let obj = {
+    name:'dhanesh',
+    age:'24'
+}
+let obj2 = obj
+console.log(obj === obj2);
+
+const user = async () =>{
+    const response = await fetch('./data.json')
+    const data = await response.json();
+    console.log(data);
+    data.map((data,index)=>{
+        let p = document.createElement('p');
+        p.append(data.title)
+        document.getElementById('demo').appendChild(p)
+    })
+}
+user()
