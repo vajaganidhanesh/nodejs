da4b2a86624e4560fa036614cfe55ea051f6f70d
vajagani dhanesh <vajaganidhanesh@gmail.com>
2022-12-18 22:21:19 +0530

    conversions in js

---
+++ conversions.js
@@ -0,0 +1,6 @@
+let str = '123';
+let number = Number(str);
+
+str = str + 23
+console.log(str, typeof str);
+
---
+++ string_method.js
@@ -0,0 +1,14 @@
+let str = 'hello world!...'
+
+let firstmethod =str.slice(0,5);
+let substring =str.length
+let lowercase = str.toLocaleLowerCase()
+let upper = str.toUpperCase()
+let startswith = str.startsWith('h')
+let endswith = str.endsWith('.')
+
+// console.log(endswith);
+console.log(substring);
+// console.log(upper);
+// console.log(lowercase);
+// console.log(firstmethod);
