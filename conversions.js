e02fba19b165213fcc8ec0928ae3068485f83226
vajagani dhanesh <vajaganidhanesh@gmail.com>
2022-12-23 13:41:34 +0530

    related to conversions are done

--- conversions.js
+++ conversions.js
@@ -1,6 +1,52 @@
-let str = '123';
+let str = '1';
 let number = Number(str);
-
-str = str + 23
+ 
+str = str + 1
 console.log(str, typeof str);
 
+// let valueForm = {}
+
+// const readvalue = (property,value )=>{
+//     valueForm[property] = value
+//     console.log(valueForm); 
+// }
+
+// document.getElementById('input').addEventListener('input',(e)=>{
+//     readvalue('input',e.target.value)
+// })
+
+// document.getElementById('btn').addEventListener('click',()=>{
+//     if(isNaN(valueForm.input) === false){
+//         console.log('its number');
+//     }
+//     else{
+//         console.log('its not a number');
+//     }
+// })
+
+// let arr = [1,2,3,4,5,6];
+// let x = arr.map((val,index)=>{
+//     if(val % 2 === 0){
+//         return val,index
+//     }
+// })
+// console.log(x);
+
+// let y = arr.filter((val,index)=>{
+//     if(val % 2 ===0){
+//         return val
+//     }
+// })
+
+// console.log(y);
+
+// let max = 0;
+
+// let z = arr.reduce((acc,curr)=>{
+//     if(acc > max)
+//     {
+//         max = acc
+//     }
+// })
+// console.log(max);
+// console.log(z);
--- relevel.js
+++ relevel.js
@@ -1,32 +1,48 @@
+// // Function candidate has to implement
+// function maximumSubarray(arr, n)
+// {
+//   for (let index = 0; index < arr.length; index++) {
+//     const element = arr[index];
+//     if(element > n)
+//     {
+//         return 2
+//     }
+//     else{
+//         return 3
+//     }
+//   }
+// }
 
+// //Driver Code
+// var t =readline();
+// while(t--)
+// {
+// 	var n = readline();
+// 	var temp = readline().trim();
+// 	var s = temp.split(" ");
+// 	for(i=0;i<n;i++)
+// 	{
+// 	    s[i]=Number(s[i]);
+// 	}
+// 	var num  = maximumSubarray(s,n);
+// 	print(num);
+// }
 
-// Function candidate has to implement
-function maximumSubarray(arr, n)
-{
-  for (let index = 0; index < arr.length; index++) {
-    const element = arr[index];
-    if(element > n)
-    {
-        return 2
-    }
-    else{
-        return 3
-    }
-  }
+// let data = '';
+// let value = false;
+
+// console.log(data === value);
+
+let number = { value: 10 };
+let anotherNum = number;
+
+function changeNumber(num) {
+  console.log("number before updation", num);
+  num.value=num.value+2;
+  console.log("number after updation", num);
 }
 
-
-//Driver Code
-var t =readline();
-while(t--)
-{
-	var n = readline();
-	var temp = readline().trim();
-	var s = temp.split(" ");
-	for(i=0;i<n;i++)
-	{
-	    s[i]=Number(s[i]);
-	}
-	var num  = maximumSubarray(s,n);
-	print(num);
-}
+console.log("the number at first", number);
+changeNumber(number);
+console.log("number after intilization of number", number);
+console.log(anotherNum);
