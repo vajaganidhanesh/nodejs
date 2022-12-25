27dddf2f55adaedcf70b878a5de547593afd7232
vajagani dhanesh <vajaganidhanesh@gmail.com>
2022-12-25 13:18:05 +0530

    object methods

--- eventDeligation.js
+++ eventDeligation.js
@@ -2,3 +2,14 @@ document.getElementById('types').addEventListener('click',(e)=>{
     console.log(e.target.id);
     window.location.href = '/'+e.target.id
 });
+
+let a = 10;
+var b = 20;
+const c = 30;
+let students = {
+    name : "dhanesh",
+    age : 24,
+    desigation : 'frontend developer'
+}
+
+console.log(students);
---
+++ object_functions.js
@@ -0,0 +1,11 @@
+let students = {
+    name : 'dhanesh',
+    age  : 24,
+    desigation : 'Frontend developer',
+}
+
+let keys = Object.keys(students)
+let values = Object.values(students)
+let entires = Object.entries(students)
+
+console.log(entires);
